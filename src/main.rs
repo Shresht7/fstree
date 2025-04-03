6ee6b27ddbac7c9a3b0295bc1e9b88767e4d17b8
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-04-04 01:12:05 +0530

    Create `run` function to house the actual run logic

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,12 @@ mod cli;
 
 fn main() {
     let args = cli::parse();
+    if let Err(e) = run(&args) {
+        eprintln!("Error: {e}");
+    }
+}
+
+fn run(args: &cli::Args) -> std::io::Result<()> {
     println!("{}", &args.path.display());
+    Ok(())
 }
