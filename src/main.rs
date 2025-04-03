61e7fec75fc97ad78b99a1b903342ef3454e682e
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-04-04 01:41:54 +0530

    Assemble the string and write once instead of writing to stdout on each entry

--- src/main.rs
+++ src/main.rs
@@ -8,15 +8,19 @@ fn main() {
 }
 
 fn run(args: &cli::Args) -> std::io::Result<()> {
+    let mut output = String::new();
+
     let walker = ignore::WalkBuilder::new(&args.path).build();
     for path in walker {
         match path {
             Ok(path) => {
                 let prefix = "  ".repeat(path.depth());
-                println!("{prefix}{}", path.file_name().to_string_lossy())
+                output.push_str(&format!("{prefix}{}\n", path.file_name().to_string_lossy()));
             }
             Err(_) => {} // Ignore errors in traversal for now
         }
     }
+
+    println!("{}", output);
     Ok(())
 }
