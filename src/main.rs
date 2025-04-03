0d93e5c0ff6d2566cdce81bb212923906811c649
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-04-04 01:29:44 +0530

    Walk the directory tree and print out the file-names

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,14 @@ fn main() {
 }
 
 fn run(args: &cli::Args) -> std::io::Result<()> {
-    println!("{}", &args.path.display());
+    let walker = ignore::WalkBuilder::new(&args.path).build();
+    for path in walker {
+        match path {
+            Ok(path) => {
+                println!("{}", path.file_name().to_string_lossy())
+            }
+            Err(_) => {} // Ignore errors in traversal for now
+        }
+    }
     Ok(())
 }
