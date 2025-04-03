40cf393940c3be26b76d445266afee72df6ef1b0
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2025-04-04 01:50:30 +0530

    Factor out the formatting logic into a separate function

--- src/main.rs
+++ src/main.rs
@@ -15,10 +15,7 @@ fn run(args: &cli::Args) -> std::io::Result<()> {
         match entry {
             Ok(entry) => {
                 let prefix = args.prefix.repeat(entry.depth());
-                output.push_str(&format!(
-                    "{prefix}{}\n",
-                    entry.file_name().to_string_lossy()
-                ));
+                output.push_str(&format_entry(prefix, &entry));
             }
             Err(_) => {} // Ignore errors in traversal for now
         }
@@ -26,4 +23,8 @@ fn run(args: &cli::Args) -> std::io::Result<()> {
 
     println!("{}", output);
     Ok(())
+}
+
+fn format_entry(prefix: String, entry: &ignore::DirEntry) -> String {
+    format!("{prefix}{}\n", entry.file_name().to_string_lossy())
 }
